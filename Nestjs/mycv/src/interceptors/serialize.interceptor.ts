7c1fddcbe74032885b006df51f37230de3816985
BarrierAntho <barrier.antho@gmail.com>
2023-07-12 15:42:42 +0200

    UPDT - [NestJs] continue course and including modules
    BODY:
    N/A
    
    TODO:
    N/A

--- Nestjs/mycv/src/interceptors/serialize.interceptor.ts
+++ Nestjs/mycv/src/interceptors/serialize.interceptor.ts
@@ -1,32 +1,37 @@
-import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
-import { Observable } from "rxjs";
-import { map } from "rxjs";
-import { plainToInstance } from "class-transformer";
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { plainToInstance } from 'class-transformer';
 
 interface ClassConstructor {
-    new (...args: any[]): {}
+  new (...args: any[]): {};
 }
 
 export function Serialize(dto: ClassConstructor) {
-    return UseInterceptors(new SerializeInterceptor(dto));
+  return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-    constructor(private dto: any) {}
+  constructor(private dto: any) {}
 
-    intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-        // Run something before a request is handled
-        // by the request handler
-        // console.log('Im running before the handler', context);
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Run something before a request is handled
+    // by the request handler
+    // console.log('Im running before the handler', context);
 
-        return handler.handle().pipe(
-            map((data: any) => {
-                // Run something before the response is sent out
-                // console.log('Im running before response is sent out', data);
-                return plainToInstance(this.dto, data, {
-                    excludeExtraneousValues: true
-                });
-            })
-        )
-    }
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        // console.log('Im running before response is sent out', data);
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
 }
--- Nestjs/mycv/src/users/users.controller.ts
+++ Nestjs/mycv/src/users/users.controller.ts
@@ -1,45 +1,98 @@
-import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+  NotFoundException,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-    constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private authService: AuthService,
+  ) {}
 
-    @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        this.userService.create(body.email, body.password);
-    }
+  // @Get('/colors/:color')
+  // setColor(@Param('color') color: string, @Session() session: any) {
+  //   session.color = color;
+  // }
 
-    // @UseInterceptors(new SerializeInterceptor(UserDto))
-    // @Serialize(UserDto)
-    @Get('/:id')
-    async findUser(@Param('id') id: string) {
-        // console.log('handler is running');
-        const user = await this.userService.findOne(parseInt(id));
-        
-        if(!user) {
-            throw new NotFoundException('User not found');
-        }
-        return user;
-    }
+  // @Get('/colors')
+  // getColor(@Session() session: any) {
+  //   return session.color;
+  // }
 
-    @Get()
-    findAllUsers(@Query('email') email: string) {
-        return this.userService.find(email);
-    }
+  // @Get('/whoami')
+  // whoAmI(@Session() session: any) {
+  //   return this.userService.findOne(session.userId);
+  // }
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user;
+  }
 
-    @Delete('/:id')
-    deleteUser(@Param('id') id:string) {
-        return this.userService.remove(parseInt(id));
-    }
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
 
-    @Patch('/:id')
-    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-        return this.userService.update(parseInt(id), body);
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  // @Serialize(UserDto)
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    // console.log('handler is running');
+    const user = await this.userService.findOne(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
 }
